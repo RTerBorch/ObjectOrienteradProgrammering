bd2b008c1ed3062865ca54910692f727d87e4f86
Robin Ter-Borch <robin.ter-borch@yh.nackademin.se>
2022-10-04 09:31:28 +0200

    Adderade uppgift 9 Roman numbers

---
+++ src/ObjectOrienteradKurs/Sprint1/Övningsuppgifter/Övningsuppgift9/PrintValue.java
@@ -0,0 +1,11 @@
+package ObjectOrienteradKurs.Sprint1.Övningsuppgifter.Övningsuppgift9;
+
+public interface PrintValue {
+
+
+    static void PrintRomanNumberValue(){
+
+    }
+
+
+}
---
+++ src/ObjectOrienteradKurs/Sprint1/Övningsuppgifter/Övningsuppgift9/RomanNumberClass.java
@@ -0,0 +1,22 @@
+package ObjectOrienteradKurs.Sprint1.Övningsuppgifter.Övningsuppgift9;
+
+public class RomanNumberClass implements PrintValue {
+
+        private int num;
+        private String symbol;
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public RomanNumberClass(int num, String symbol) {
+        this.num = num;
+        this.symbol = symbol;
+    }
+
+
+}
